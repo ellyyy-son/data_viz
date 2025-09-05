a7ef6a34c6a0e620f09f3dc3d88173680030c565
ellyyy-son <167102704+ellyyy-son@users.noreply.github.com>
2025-09-05 20:57:24 +0800

    Update 2_LDA.py

--- pages/2_LDA.py
+++ pages/2_LDA.py
@@ -1,6 +1,8 @@
 import streamlit as st  
 from streamlit import components
 
+st.set_page_config(
+    layout="wide")
 lda = 'static/lda.html'
 with open(lda, 'r', encoding='utf-8') as f:  
     html_string = f.read()
