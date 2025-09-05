9d26ed7a64e149ea5c38a91d65f2da73e59b1267
ellyyy-son <167102704+ellyyy-son@users.noreply.github.com>
2025-09-05 20:28:49 +0800

    Added LDA

---
+++ pages/2_LDA.py
@@ -0,0 +1,8 @@
+import streamlit as st  
+from streamlit import components
+
+lda = 'static/lda.html'
+with open(lda, 'r', encoding='utf-8') as f:  
+    html_string = f.read()
+
+components.v1.html(html_string, width=1300, height=800)
